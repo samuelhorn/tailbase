115ac7e540a911a4c5bb97388a8529c71c2d074a
Samuel Horn af Rantzien <samuel@samuelhorn.com>
2020-08-23 01:39:26 +0200

    meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,18 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      // Twitter Card
+      {name: 'twitter:card', content: 'summary'},
+      {name: 'twitter:title', content: 'Tailbase'},
+      {name: 'twitter:description', content: 'Tailwind config generator for a typygraphic scale with vertical rythm.'},
+      {name: 'twitter:image', content: 'https://tailbase.samuelhorn.com/summary.png'},
+      // Facebook OpenGraph
+      {property: 'og:title', content: 'Tailbase'},
+      {property: 'og:site_name', content: 'Tailbase'},
+      {property: 'og:type', content: 'website'},
+      {property: 'og:image', content:  'https://tailbase.samuelhorn.com/summary.png'},
+      {property: 'og:description', content: 'Tailwind config generator for a typygraphic scale with vertical rythm.'}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
